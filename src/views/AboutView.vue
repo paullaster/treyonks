2d2917d980078cbb89221f72e75e451add85906d
paullaster <paullasterokoth98@gmail.com>
2024-09-18 07:44:56 +0300

    update mobile view

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,17 +1,17 @@
 <template>
   <section class="about">
     <h2>{{ Aboutheading }}</h2>
-    <p class="description">
-      <span>
+    <p class="about_description">
+      <span class="key_text">
         Vision:
       </span>
-      <span>
+      <span class="value_text">
         To revolutionize the business landscape through innovative Dynamics solutions.
       </span>
     </p>
-    <p class="description">
-      <span>Mission:</span>
-      <span>
+    <p class="about_description">
+      <span class="key_text">Mission:</span>
+      <span class="value_text">
         We are more than just a service provider. We are a strategic partner, dedicated to empowering
       businesses with tailored Dynamics solutions that drive exceptional results. Our unwavering
       commitment to excellence and our passion for innovation fuel our mission to help clients
