8477e39963fe46e096e9726cea8da958ed8aae13
paullaster <paullasterokoth98@gmail.com>
2024-09-18 06:57:09 +0300

    update mobile view

--- src/components/ServicesComponents.vue
+++ src/components/ServicesComponents.vue
@@ -1,53 +1,60 @@
 <template>
-    <h2 class="title">Our Services</h2>
-    <p>
-      Transform your business with our unparalleled Dynamics solutions. From strategic finance and
+   <section :class="onServicePage ? `services` : `services`">
+    <h2 class="section-heading">Our Services</h2>
+    <p class="service-intro">
+      Transform your business with our unparalleled Microsoft 365 Dynamics and CRM solutions. From strategic finance and
       HR to streamlined operations and customer engagement, we deliver tailored, innovative
       solutions that empower you to achieve exceptional results.
     </p>
-    <div>
-      <div>
-        <v-img :src="financesln" />
-        <p>Master Your Finances</p>
+    <div class="services-container">
+      <div class="service-card">
+        <v-img :src="financesln" class="rounded-lg"></v-img>
+        <h5>Master Your Finances</h5>
         <p>
           Our tailored finance management solutions, powered by Dynamics 365, provide you with the
           tools to streamline your operations, optimize cash flow, and make data-driven decisions.
           With real-time insights, automated workflows, and seamless integration, we help you
           achieve financial excellence.
         </p>
-        <v-btn> Talk to an expert </v-btn>
+        <v-btn class="rounded-pill" size="x-large" density="default" variant="outlined" block > Talk to an expert </v-btn>
       </div>
-      <div>
-        <v-img :src="hrslns" />
-        <p>Elevate Your Workforce</p>
+      <div class="service-card">
+        <v-img :src="hrslns" class="rounded-lg"/>
+        <h5>Elevate Your Workforce</h5>
         <p>
           Our tailored HR solutions, powered by Dynamics 365, provide you with the tools to
           streamline your HR processes, optimize employee engagement, and foster a thriving
           workplace culture. With automated workflows, advanced analytics, and seamless integration,
           we help you attract, develop, and retain top talent.
         </p>
-        <v-btn> Talk to an expert </v-btn>
+        <v-btn class="rounded-pill" size="x-large" density="default" variant="outlined" > Talk to an expert </v-btn>
       </div>
-      <div>
-        <v-img :src="crmsln" />
-        <p>CRM Solutions</p>
+      <div class="service-card">
+        <v-img :src="crmsln" class="rounded-lg"/>
+        <h5>CRM Solutions</h5>
         <p>
           Our tailored CRM solutions, provide you with the tools to nurture
           lasting relationships, drive sales, and deliver exceptional customer experiences. With
           automated workflows, advanced analytics, and seamless integration, we help you build
           customer loyalty and achieve your business goals.
         </p>
-        <v-btn> Talk to an expert </v-btn>
+        <v-btn class="rounded-pill" size="x-large" density="default" variant="outlined" > Talk to an expert </v-btn>
       </div>
     </div>
+   </section>
 </template>
   
   <script setup>
 import financesln from '@/assets/finance_sln.png'
 import hrslns from '@/assets/Light+Objects+1.png'
 import crmsln from '@/assets/crm_sln.png'
+import { computed } from 'vue';
+
+
+const onServicePage = computed(() => {
+  return window.location.href.includes('/services');
+})
+
 </script>
-  
-  <style scoped>
-</style>
+
   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,77 +1,66 @@
 <template>
-   <div class="landing">
-    <div>
+  <div class="landing">
+    <div class="landing-section-1">
       <h3><span>Discover</span> <span>Innovation</span></h3>
-      <p>
-        Empowering your business with personalized Dynamics solutions.
-      </p>
+      <p>Empowering your business with personalized Microsoft 365 Dynamics and CRM solutions.</p>
       <div class="call-to-action">
-        <v-btn :color="ColorsHelper.getColor('primary')" large to="/about" class="mr-6">Learn More</v-btn>
+        <v-btn
+          :color="ColorsHelper.getColor('primary')"
+          large
+          to="/about"
+          class="mr-6 rounded-pill"
+          desity="default"
+          size="x-large"
+          elevation="8"
+          >Learn More</v-btn
+        >
       </div>
     </div>
     <div class="hero-image">
-      <img src="@/assets/Light+Objects+1.png" alt="Hero">
+      <v-img :src="HomePageDecoImg" alt="Hero" height="480px" class="rounded-lg"></v-img>
     </div>
   </div>
-        <ServiceComponent />
-        <BookDemo />
-        <AboutViewVue />
+  <ServiceComponent />
+  <BookDemo />
+  <AboutViewVue />
 </template>
 
 <script setup>
 import ServiceComponent from '@/components/ServicesComponents.vue'
-import BookDemo from '@/components/BookDemo.vue';
+import BookDemo from '@/components/BookDemo.vue'
 import AboutViewVue from './AboutView.vue'
-import { onMounted, ref } from 'vue';
-import { useDisplay } from 'vuetify/lib/framework.mjs';
-import ColorsHelper from '@/utils/ColorsHelper';
-
+import { onMounted, ref } from 'vue'
+import { useDisplay } from 'vuetify/lib/framework.mjs'
+import ColorsHelper from '@/utils/ColorsHelper'
+import HomePageDecoImg from "@/assets/Light+Objects+1.png"
 
 // VUETIFY UTILS
-const {
-  mdAndDown
-} = useDisplay();
+const { mdAndDown } = useDisplay()
 
-const text = "At Treyonks Technology Limited, we specialize in providing comprehensive support and customized solutions for Dynamics Business Central and Dynamics CRM.";
-const displayedText = ref("");
-const wordIndex = ref(0);
-const words = ref([]);
+const text =
+  'At Treyonks Technology Limited, we specialize in providing comprehensive support and customized solutions for Dynamics Business Central and Dynamics CRM.'
+const displayedText = ref('')
+const wordIndex = ref(0)
+const words = ref([])
 
 onMounted(() => {
-  words.value = text.split(" ");
+  words.value = text.split(' ')
   console.log(words.value)
-    typeWords();
+  typeWords()
 })
 
 function typeWords() {
-      if (wordIndex.value < words.value.length) {
-        displayedText.value += words.value[wordIndex.value] + " ";
-        wordIndex.value++;
-        setTimeout(typeWords, 300); // Adjust the speed here
-      } else {
-        setTimeout(resetTyping, 120000); // Pause before restarting
-      }
-    };
+  if (wordIndex.value < words.value.length) {
+    displayedText.value += words.value[wordIndex.value] + ' '
+    wordIndex.value++
+    setTimeout(typeWords, 300) // Adjust the speed here
+  } else {
+    setTimeout(resetTyping, 120000) // Pause before restarting
+  }
+}
 function resetTyping() {
-      displayedText.value = "";
-      wordIndex.value = 0;
-      typeWords();
-    }
-
+  displayedText.value = ''
+  wordIndex.value = 0
+  typeWords()
+}
 </script>
-<style scoped>
-.landing {
-  background: #dcdcdc;
-  text-align: center;
-  color: #141414;
-}
-.hero-image {
-  position: relative;
-  height: 300px;
-}
-.hero-image img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
-</style>
