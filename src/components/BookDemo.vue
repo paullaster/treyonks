43909e5f902f06a0fa052ce71bad997d4579b925
paullaster <paullasterokoth98@gmail.com>
2024-09-19 12:41:46 +0300

    desktop view

--- src/components/BookDemo.vue
+++ src/components/BookDemo.vue
@@ -1,14 +1,17 @@
 <template>
-    <section class="book_demo">
+    <section :class=" mdAndDown ? 'demo_page book_demo' : ' demo_page book_demo_desktop'">
        <h4>Book a free Consultation</h4>
        <div class="book_button">
-           <v-btn class="rounded-pill" size="x-large" block :color="ColorsHelper.getColor('header')">Book Now</v-btn>
+           <v-btn class="rounded-pill" size="x-large" :block="mdAndDown" :color="ColorsHelper.getColor('header')" width="400px">Book Now</v-btn>
        </div>
     </section>
 </template>
 
 <script setup>
 import ColorsHelper from '@/utils/ColorsHelper';
+import { useDisplay } from 'vuetify/lib/framework.mjs';
+
+const { mdAndDown } = useDisplay();
 
 </script>
 
